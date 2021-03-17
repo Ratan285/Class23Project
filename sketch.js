ef0e901c3a6c3ef274d9f1c9fdc64192a6c2e03d
Ratan285 <ratanprasanna3@gmail.com>
2021-03-17 16:41:55 +0530

    changes2

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function preload()
 
 function setup() {
 	createCanvas(800, 700);
-	rectMode(CENTER);
+	rectMode(CENTER); 
 	
 
 	packageSprite=createSprite(width/2, 80, 10,10);
@@ -34,14 +34,11 @@ function setup() {
 	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
 	World.add(world, packageBody);
 	
-
-	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
  	boxPosition=width/2-100
  	boxY=610;
-
 
  	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
  	boxleftSprite.shapeColor=color(255,0,0);
